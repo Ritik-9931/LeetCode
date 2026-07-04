032547d3c9f773f0214f092d5da1175b59aef9d0
Ritik-9931 <ritikraushan7372@gmail.com>
2026-07-04 07:47:29 +0530

    Time: 62 ms (5.99%), Space: 55.7 MB (93.76%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const a = init;
+    return {
+        increment: function(){
+            return ++init;
+        },
+        decrement: function(){
+            return --init;
+        },
+        reset: function(){
+            return init = a;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
