84a79d90453e62180b6e75a3b5153208a6cba1b2
Ritik-9931 <ritikraushan7372@gmail.com>
2026-07-04 07:55:26 +0530

    Time: 53 ms (15.75%), Space: 53.7 MB (76.55%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let accum = init;
+    nums.forEach((element) => {
+        accum = fn(accum, element)
+    })
+
+    return accum;
+};
