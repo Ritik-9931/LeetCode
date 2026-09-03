363579d7d76b4cc731fd55b83561e9498bab681e
Ritik-9931 <ritikraushan7372@gmail.com>
2026-09-03 17:57:42 +0530

    Time: 5 ms (82.33%), Space: 46 MB (52.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int num = x;
+        int res = 0;
+
+        while(x != 0){
+            int rem = x%10;
+            res = res*10 + rem;
+            x = x/10;
+        }
+
+        return num == res;
+    }
+}
